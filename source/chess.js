d09c112b40faf67ded2d51ef122e9be2eaa8c6a1
Артем Бакулев <artyombakulev@MacBook-Pro-Artem.local>
2019-09-01 22:31:13 +0300

    Add empty lines

--- source/chess.js
+++ source/chess.js
@@ -9,13 +9,17 @@ const chess = size => {
     if (isNaN((size = parseInt(size))) || size < 2) return null;
     const board = [];
     const rowPart = '* ';
+
     const row = rowPart.repeat(size / 2 + 1).split('');
     const rowFirst = row.slice(0, size);
     const rowSecond = row.slice(1, size + 1);
+
     rowFirst.push('\n');
     rowSecond.push('\n');
+
     for (let i = 0; i < size; i++) {
         i % 2 !== 1 ? board.push(rowFirst.join('')) : board.push(rowSecond.join(''));
     }
+
     return board.join('');
 };
